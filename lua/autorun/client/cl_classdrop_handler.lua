f24926dda94f401c91b383ed1a1c8aaab2e4699d
Tim Goll <tim@timgoll.de>
2020-02-10 18:56:49 +0100

    switch to reworked targetID system

--- lua/autorun/client/cl_classdrop_handler.lua
+++ lua/autorun/client/cl_classdrop_handler.lua
@@ -6,33 +6,33 @@ net.Receive("TTTCSetClassCooldownTS", function()
 end)
 
 -- target ID
-hook.Add("TTTRenderEntityInfo", "ttt2_class_drop_render_entity_info", function(data, params)
+hook.Add("TTTRenderEntityInfo", "ttt2_class_drop_render_entity_info", function(tData)
 	local client = LocalPlayer()
+	local ent = tData:GetEntity()
 
-	if data.distance > 100 or data.ent:GetClass() ~= "tttc_classdrop" then return end
 	if not IsValid(client) or not client:IsTerror() or not client:Alive() then return end
+	if tData:GetEntityDistance() > 100 or not IsValid(ent) or ent:GetClass() ~= "tttc_classdrop" then return end
 
-	local hd = CLASS.GetClassDataByIndex(data.ent:GetNWInt("customClass"))
+	local hd = CLASS.GetClassDataByIndex(ent:GetNWInt("customClass"))
 	local pName = hd and CLASS.GetClassTranslation(hd) or ""
 
-	params.drawInfo = true
-	params.displayInfo.key = input.GetKeyCode(input.LookupBinding("+use"))
-	params.displayInfo.title.text = LANG.GetParamTranslation("ttt_class_title", {class = pName})
-	params.displayInfo.subtitle.text = LANG.GetParamTranslation("ttt_pickup_class", {usekey = Key("+use", "USE")})
+	-- enable targetID rendering
+	tData:EnableText()
+	tData:EnableOutline()
+	tData:SetOutlineColor(client:GetRoleColor())
+
+	tData:SetTitle(LANG.GetParamTranslation("ttt_class_title", {class = pName}))
+	tData:SetSubtitle(LANG.GetParamTranslation("ttt_pickup_class", {usekey = Key("+use", "USE")}))
+	tData:SetKeyBinding("+use")
 
 	-- add class description to targetID if provided
 	if hd and hd.lang and hd.lang.desc then
 		desc_wrapped = draw.GetWrappedText(LANG.TryTranslation("tttc_class_" .. hd.name .. "_desc"), 400, "TargetID_Description")
 
 		for i = 1, #desc_wrapped do
-			params.displayInfo.desc[#params.displayInfo.desc + 1] = {
-				text = tostring(desc_wrapped[i]), color = COLOR_WHITE
-			}
+			tData:AddDescriptionLine(desc_wrapped[i])
 		end
 	end
-
-	params.drawOutline = true
-	params.outlineColor = client:GetRoleColor()
 end)
 
 hook.Add("Initialize", "ttt2_classdrop_init_lang", function()
