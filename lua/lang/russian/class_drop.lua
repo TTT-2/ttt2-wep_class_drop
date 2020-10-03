28b844f660f5673667acf5cd018dda4018a0ad79
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:08:52 +0500

    Create class_drop.lua

---
+++ lua/lang/russian/class_drop.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt_class_title"] = "Класс: {class}"
+L["ttt_pickup_class"] = "Нажмите [{usekey}], чтобы подобрать класс"
+L["ttt_drop_class_help"] = "Отбросить класс"
