8d8c03d7a9da468a3b75a5020e1ec1c5a54553e8
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 02:34:51 +0100

    Create class_drop.lua
    french translation : class_drop

---
+++ lua/terrortown/lang/french/class_drop.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["ttt_class_title"] = "Classe: {class}"
+L["ttt_pickup_class"] = "Appuyez sur [{usekey}] pour choisir une classe"
+L["ttt_drop_class_help"] = "Laissez la classe"
